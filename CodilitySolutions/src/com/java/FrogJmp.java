296bfe7b8bc927ed17abd0aa16ab3b2eb9a0d6a9
jamesirungu <irungu.m.j@gmail.com>
2022-03-10 00:23:58 +0300

    added FrogJmp solution

---
+++ CodilitySolutions/src/com/java/FrogJmp.java
@@ -0,0 +1,13 @@
+package com.java;
+
+public class FrogJmp {
+	public static void main(String[] args) {
+		System.out.println(FrogJmp.solution(10,85,30));
+	}
+	public static int solution(int X, int Y, int D) {
+		
+		int distance =Y-X;
+		int stepCount=(int) Math.ceil((double)distance/D);
+		return stepCount;
+	}
+}
